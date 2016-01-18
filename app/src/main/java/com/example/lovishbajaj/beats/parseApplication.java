bf391aa2471420486ede3b2eba5afb306f8d6ade
Lovish Bajaj <bajaj.lovish@gmail.com>
2016-01-18 22:00:22 +0530

    first commit

---
+++ app/src/main/java/com/example/lovishbajaj/beats/parseApplication.java
@@ -0,0 +1,13 @@
+package com.example.lovishbajaj.beats;
+
+import android.app.Application;
+
+/**
+ * Created by lovishbajaj on 07/01/16.
+ */
+public class parseApplication extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+}
